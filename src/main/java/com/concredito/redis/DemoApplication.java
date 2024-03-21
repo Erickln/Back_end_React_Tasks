adc2d2760a4aa30627b62c9ae9305a8efafe8730
Erickln <wall.e50@hotmail.com>
2024-03-21 01:50:32 -0700

    FCommit

---
+++ src/main/java/com/concredito/redis/DemoApplication.java
@@ -0,0 +1,51 @@
+package com.concredito.redis;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.concredito.redis.demo.entity.User;
+import com.concredito.redis.demo.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/user")
+public class DemoApplication {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping
+	public User save(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+
+	@GetMapping
+	public List<Object> list() {
+		return userRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public User findById(String id) {
+		return userRepository.findById(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(String id) {
+		return userRepository.delete(id);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/concredito/redis/demo/repository/UserRepository.java
@@ -0,0 +1,37 @@
+package com.concredito.redis.demo.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.concredito.redis.demo.entity.User;
+
+@Repository
+public class UserRepository {
+
+    public static final String HASH_KEY = "User";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @SuppressWarnings("null")
+    public User save(User user) {
+        redisTemplate.opsForHash().put(HASH_KEY, user.getId(), user);
+        return user;
+    }
+
+    public List<Object> findAll() {
+        return redisTemplate.opsForHash().values(HASH_KEY);
+    }
+
+    @SuppressWarnings("null")
+    public User findById(String id) {
+        return (User) redisTemplate.opsForHash().get(HASH_KEY, id);
+    }
+
+    public String delete(String id) {
+        redisTemplate.opsForHash().delete(HASH_KEY, id);
+        return "User removed !!";
+    }
+}
